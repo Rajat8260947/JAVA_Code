9b3f4b728d640636060d638973419d694fe3e293
Rajat <rajatkumarsaho638@gmail.com>
2024-07-24 20:21:09 +0530

    first commit

---
+++ src/java_pratice/Rajat/StaticExaple/Human.java
@@ -0,0 +1,19 @@
+package java_pratice.Rajat.StaticExaple;
+
+public class Human {
+
+    int age ;
+    String name;
+    int salary;
+    boolean married;
+
+    public  Human(int age,String name,int salary, boolean married){
+        this.age= age;
+        this.name = name;
+        this.salary=salary;
+        this.married=married;
+
+    }
+
+
+}
---
+++ src/java_pratice/Rajat/StaticExaple/Main.java
@@ -0,0 +1,7 @@
+package java_pratice.Rajat.StaticExaple;
+
+public class Main {
+    public static void main(String[] args) {
+       Human kunal = new Human(22,"kunal",1000,true);
+    }
+}
